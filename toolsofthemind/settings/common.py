b9b218a9c0121db6ce0a1a5c61099af806978e00
Lawrence McDaniel <lpm0073@gmail.com>
2021-09-11 08:31:38 -0500

    add separate template folders for lms, cms

--- toolsofthemind/settings/common.py
+++ toolsofthemind/settings/common.py
@@ -4,14 +4,15 @@ Common Pluggable Django App settings
 from path import Path as path
 
 PACKAGE_ROOT = path(__file__).abspath().dirname().dirname()  # /toolsofthemind
-REPO_ROOT = PACKAGE_ROOT.dirname()  # toolsofthemind-digital-learning-openedx
-TEMPLATES_DIR = PACKAGE_ROOT / "templates"
+REPO_ROOT = PACKAGE_ROOT.dirname()  # edx-toolsofthemind
+LMS_TEMPLATES_DIR = PACKAGE_ROOT / "lms" / "templates"
+CMS_TEMPLATES_DIR = PACKAGE_ROOT / "cms" / "templates"
 
 
 def plugin_settings(settings):
     """
     Injects additional Django settings into lms.envs.common
     """
-    # Add the template directory for this package to
+    # Add the template directories for this package to
     # to the search path for Mako.
-    settings.MAKO_TEMPLATE_DIRS_BASE.extend([TEMPLATES_DIR])
+    settings.MAKO_TEMPLATE_DIRS_BASE.extend([LMS_TEMPLATES_DIR, CMS_TEMPLATES_DIR])
