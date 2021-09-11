46713bcad7082f9d020d79841a3521dbd139226f
Lawrence McDaniel <lpm0073@gmail.com>
2021-09-11 14:26:03 -0500

    code methods to build menu list of dicts

--- toolsofthemind/utils.py
+++ toolsofthemind/utils.py
@@ -6,7 +6,12 @@ toolsofthemind utils.
 """
 
 # Django
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.db.models import Subquery
+
+# Open edX
+from lms.djangoapps.courseware.courses import get_course
 
 # This repo
 from toolsofthemind.models import (
@@ -17,14 +22,91 @@ from toolsofthemind.models import (
 )
 
 
+@login_required
 @ensure_csrf_cookie
-def get_menu_categories(request):
+def get_menu_categories(request, courses):
     """
     Return a Django ORM dictionary of the Tools of the Mind courses that are available
     to this user.
-    """
-    course_groups_list = TOMStudentCourseGroups.objects.filter(user=request.user)
-    course_subgroups_list = TOMCourseSubgroups.objects.filter(course_groups__in=course_groups_list)
-    course_menu = TOMCourseMenu.objects.filter(course_subgroup__in=course_subgroups_list)
 
-    return course_menu
+    inputs:
+    -------
+    request: a standard edx-platform Django request object
+
+    courses: list of CourseOverview objects reeturned by lms.djangoapps.courseware.courses.get_courses()
+             the list of courses deemed available to the user based on whatever policies
+             are presently in place to grant/restrict access to each course run.
+
+    see: openedx.core.djangoapps.courses.models.CourseOverview
+    """
+    course_groups = TOMStudentCourseGroups.objects.filter(user=request.user)
+    retval = []
+
+    for group in course_groups:
+        retval.append(
+            {"id": group.id, "course_group": group.course_group, "subgroups": _get_subgroups_for_group(group, courses)}
+        )
+
+    return retval
+
+
+def _get_subgroups_for_group(group, courses):
+    """
+    Returns the subgroup list for this group, each enhanced with
+    its respective courses list.
+    """
+    subgroups = TOMCourseSubgroups.objects.filter(course_group=group).order_by("ordinal_position")
+    retval = []
+
+    for subgroup in subgroups:
+        retval.append(
+            {
+                "id": subgroup.id,
+                "ordinal_position": subgroup.ordinal_position,
+                "course_subgroup": subgroup.course_subgroup,
+                "courses": _get_courses_for_subgroup(subgroup, courses),
+            }
+        )
+
+    return retval
+
+
+def _get_courses_for_subgroup(subgroup, courses):
+    """
+    Returns the cross of the contents of the subgroup's
+    course list and the courses list input parameter
+
+    inputs:
+    ------
+    subgroup: a Django ORM TOMCourseSubgroups object
+
+    courses: the courses which this user can see.
+    """
+    courses_in_menu = TOMCourseMenu.objects.filter(course_subgroup=subgroup).values("course")
+    retval = []
+
+    for course_menu_item in courses_in_menu:
+        course_locator = course_menu_item["course"]
+        course = get_course(course_locator)
+        if course in list(courses):
+            retval.append(course)
+
+    return retval
+
+
+def test(request):
+    """
+    course_groups = TOMCourseGroups.objects.all()
+
+    # get a list of the course groups assigned to this user.
+    course_groups_list = TOMStudentCourseGroups.objects.filter(user=request.user)
+
+    # get a list of all course sub-groups related to the user's course groups
+    course_subgroups_list = TOMCourseSubgroups.objects.filter(
+        course_group__in=Subquery(course_groups_list.values("course_group"))
+    )
+
+    # get a list of all courses related to the course sub-groups
+    course_menu = TOMCourseMenu.objects.filter(course_subgroup__in=Subquery(course_subgroups_list.values("id")))
+    course_menu = list(course_menu.values("id", "course_subgroup_id", "course_id"))
+    """
